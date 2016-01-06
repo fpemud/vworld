b55e9ca3f87cc958e8998b85b8378a9d054e73b2
fpemud <fpemud@sina.com>
2016-01-06 17:53:57 +0800

    fix

---
+++ src/objs/stock.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+
+class Stock:
+	def __init__(self):
+		pass
+
+	@property
+			
---
+++ src/objs/world.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+
+class World:
+
+	def __init__(self):
+		pass
+
+	def getDateTime(self):
+		pass
+
+	def changeDateTime(self):
+		pass
+
+	###########################################################################
+
+	@property
+	def stockMarkets(self):
+		return []
