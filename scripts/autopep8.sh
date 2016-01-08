0be56e83ebe736e6a9d7b0fc5f5d6d2bddedcce6
fpemud <fpemud@sina.com>
2016-01-08 15:58:34 +0800

    fix

--- scripts/autopep8.sh
+++ scripts/autopep8.sh
@@ -5,4 +5,4 @@ BIN_FILES="${ROOTDIR}/vworld-server ${ROOTDIR}/vwctl"
 LIB_FILES="$(find ${ROOTDIR}/src -name '*.py' | tr '\n' ' ')"
 
 autopep8 -ia --ignore=E402,E501 ${BIN_FILES}
-autopep8 -ia --ignore=E402,E501 ${LIB_FILES}
+autopep8 -ia --ignore=E501 ${LIB_FILES}
