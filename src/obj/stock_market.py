766ef109ca9659b4d26e385cfb2c2d81c974633a
fpemud <fpemud@sina.com>
2016-01-08 10:30:31 +0800

    fix

--- src/obj/stock_market.py
+++ src/obj/stock_market.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 
-class StockMarket:
+class StockMarket(_ObjBase):
+
+	def __init__(self, world, name):
+		super(StockMarket, self).__init__(world, name)
+		self._name = name
 
 	@property
-	def stocks(self):
+	def name(self):
+		return self._name
+
+	@property
+	def allStocks(self):
 		return []
-	
+
+	@property
+	def stock(self, stock_id):
+		return None
